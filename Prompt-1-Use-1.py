7de52483c3f49a341aaa72377e340d2e6be71168
Jorge Devops <bk11iscool@gmail.com>
2025-06-06 16:51:47 -0700

    Solved interview question for file prompt-1-use-1

--- Prompt-1-Use-1.py
+++ Prompt-1-Use-1.py
@@ -22,3 +22,24 @@
 # You may use built-in libraries for string manipulation.
 # Must run efficiently for up to 10,000 words.
 
+
+inputString = "The quick brown fox jumps over the lazy dog. The dog was not amused."
+substringList = list(map(lambda i: i.replace(".", ""), inputString.split()))
+valueList = [["Padding", 1]]
+if(len(substringList) > 2):
+    for i in substringList:
+        matchFound = False
+        for j in valueList:
+            if i.lower() == j[0]:
+                j[1] = j[1] + 1
+                matchFound = True
+        if matchFound == False:
+            valueList.append([i.lower(), 1])
+    sortedValueList = sorted(valueList,key=lambda i: i[1])
+    print(sorted(valueList,key=lambda i: i[1]))
+    print(sortedValueList[-1])
+    print(sortedValueList[-2])
+    print(sortedValueList[-3])
+else:
+    for i in substringList:
+        print(i)
